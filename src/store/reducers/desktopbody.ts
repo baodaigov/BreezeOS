1a20b1dc3fd0d3ee5dc891e292f06876638865a6
daothanhminh <khangbietcode1000@gmail.com>
2023-11-19 00:19:43 +0700

    Commit

--- src/store/reducers/desktopbody.ts
+++ src/store/reducers/desktopbody.ts
@@ -2,10 +2,12 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface StateType {
   active: boolean;
+  hide: boolean;
 }
 
 const initialState: StateType = {
   active: false,
+  hide: false,
 };
 
 const desktopBodySlice = createSlice({
@@ -15,9 +17,13 @@ const desktopBodySlice = createSlice({
     setDesktopBodyActive: (state, action: PayloadAction<boolean>) => {
       state.active = action.payload;
     },
+    setDesktopBodyHide: (state, action: PayloadAction<boolean>) => {
+      state.hide = action.payload;
+    },
   },
 });
 
-export const { setDesktopBodyActive } = desktopBodySlice.actions;
+export const { setDesktopBodyActive, setDesktopBodyHide } =
+  desktopBodySlice.actions;
 
 export default desktopBodySlice.reducer;
